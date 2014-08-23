1c0631cf08625b0a77d317a3eb4761462cc08b30
Dieguisimo <diegofernando83@gmail.com>
2014-08-23 17:35:57 -0500

    Header fixed y contenedores se adaptan al Tamano del browser

--- source/js/app.js
+++ source/js/app.js
@@ -20,3 +20,30 @@ require.config({
 	urlArgs: "bust=" + (new Date()).getTime(),
 
 });
+
+require([
+	'jquery',
+	'backbone', 
+	'app/Router'
+	], 
+
+	function ($, Backbone, Router) {
+
+
+		// Metodo para liberar memoria y remover vistas
+		Backbone.View.prototype.close = function() {
+	        console.log('cerrando view ' + this.cid);
+
+	        if (this.onClose) this.onClose();            
+
+	        this.remove();
+	        this.unbind();   
+	    };
+
+
+
+		window.router = new Router();
+	    Backbone.history.start();
+	}
+	
+);
---
+++ source/js/app/Router.js
@@ -0,0 +1,39 @@
+define(function (require) {
+
+	"use strict";
+
+	var $                = require('jquery'),
+		Backbone         = require('backbone');
+
+
+	return Backbone.Router.extend({
+
+		initialize: function() {
+			console.log('iniciando router');
+
+			// function jqUpdateSize(){
+			//     // Get the dimensions of the viewport
+			//     var width = $(window).width();
+			//     var height = $(window).height();
+
+			//     $('#jqWidth').html(width);      // Display the width
+			//     $('#jqHeight').html(height);    // Display the height
+			// };
+			$(document).ready(this.updateSize);    // When the page first loads
+			$(window).resize(this.updateSize);     // When the browser changes size
+		},
+
+		updateSize: function() {
+			var width = $(window).width();
+		    var height = $(window).height();
+
+		    $('.contenedor').height(height);
+		    console.log(width)
+		    console.log('ALTO')
+		    console.log(height)
+		},
+
+
+	});
+
+});
